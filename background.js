51314a63382c0cad8208dc8fe6e5a3f3b7e06b39
Zebulun McNeill <zeb.mcn@gmail.com>
2022-03-25 14:22:04 -0500

    getting more config setup

--- background.js
+++
@@ -1,8 +0,0 @@
-const color = '#3AA757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
-  })
-});
